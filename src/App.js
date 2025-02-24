ddadc62301b9753d60a23f511cdf9abce0afe144
Shubham Upadhyay <shubham.1822en1142@kiet.edu>
2025-02-24 16:05:54 +0530

    update 24/02

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element=<Home /> />
-        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
